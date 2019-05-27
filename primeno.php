fddc6fcba37d393dc69f1867592ca0526d49d3d7
Anisha-git <50451104+Anisha-git@users.noreply.github.com>
2019-05-27 14:53:55 +0530

    Update primeno.php

--- primeno.php
+++ primeno.php
@@ -6,11 +6,16 @@ function primenoMultiply(){
 
 	for ($i = 1; $i < 12; $i++) {
   	for ($j = 1; $j < 12; $j++) {
+	if($i%$j == 0){
+	break;
+		
+	}else{
      if ($j == 1) {
      $output = echo str_pad($i*$j, 2, " ", STR_PAD_LEFT);
      } else {
      $output =  echo str_pad($i*$j, 4, " ", STR_PAD_LEFT);
      }
+	}
   }
   
  $output = echo "\n";
